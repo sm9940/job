2a3c0df1effc509b13a02116248e779543d8e119
sm9940 <elation4927@gmail.com>
2025-05-07 17:54:58 +0900

    250507

---
+++ fast/project/hello/main.py
@@ -0,0 +1,30 @@
+from typing import Union
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"GET": "fast"}
+
+@app.post("/")
+def create_root():
+    return {"POST": "fast"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
